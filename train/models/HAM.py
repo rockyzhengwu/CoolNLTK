8ca074775bafec0d8621331f81cc390bd542982a
wuzheng <zhengwu@midday.me>
2018-03-13 18:09:50 +0800

    update HAM

---
+++ train/models/HAM.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib  import rnn
+
+
+class HAM(object):
+
+    def __init__(self, config):
+
+        pass
+
